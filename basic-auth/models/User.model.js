a5519ea1798f4e42eac6f5e606f874522c99755e
Cule219 <culaficstefan@gmail.com>
2021-08-28 11:11:02 -0600

    example code

--- basic-auth/models/User.model.js
+++ basic-auth/models/User.model.js
@@ -4,12 +4,16 @@ const { Schema, model } = require("mongoose");
 const userSchema = new Schema({
   username: {
     type: String,
-    unique: true, // -> Ideally, should be unique, but its up to you
+    unique: [true, "Username is already taken"], // -> Ideally, should be unique, but its up to you
   },
   email: {
     type: String,
     required: [true, "Please enter email"],
-    unique: true,
+    unique: [true, "Email is already registered"],
+    match: [
+      /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/,
+      "Please enter a valid email",
+    ],
   },
   password: String,
 });
--- basic-auth/routes/auth.routes.js
+++ basic-auth/routes/auth.routes.js
@@ -8,22 +8,83 @@ router.get("/auth/signup", (req, res, next) => {
   res.render("auth/signup");
 });
 
+router.get("/auth/login", (req, res, next) => {
+  console.log("Our session:", req.session);
+  if (req.session && req.session.user) {
+    // req.session?.user
+    res.render("index", req.session.user);
+  } else {
+    res.render("auth/login");
+  }
+});
+
+router.post("/auth/login", (req, res, next) => {
+  // console.log("body content:", req.body);
+  const { username, password } = req.body;
+
+  if (username === "" || password === "") {
+    res.render("index", {
+      errorMessage: "Please enter both username and password",
+    });
+  }
+  User.findOne({ username }).then((userFromDB) => {
+    // {} null
+    if (!userFromDB) {
+      // for the sake of security we will have a generic message
+      res.status(500).render("index", {
+        errorMessage: "Incorrect data",
+      });
+    } else if (bcrypt.compareSync(password, userFromDB.password)) {
+      // console.log(req.session.user);
+      req.session.user = userFromDB;
+      res.status(200).render("index", userFromDB);
+    } else {
+      res.status(500).render("index", {
+        errorMessage: "Incorrect data",
+      });
+    }
+  });
+});
+
 router.post("/auth/signup", (req, res, next) => {
-  console.log("Body", req.body);
+  // console.log("Body", req.body);
   //   res.render("auth/signup");
   const { username, password, email } = req.body;
 
-  User.find({ email }).then((userFromDB) => {
-    if (!userFromDB) {
-      const hashedPassword = bcrypt.hashSync(password, 10);
-      User.create({ username, password: hashedPassword, email }).then(
-        (responseFromDB) => {
-          res.render("index", responseFromDB);
-        }
-      );
-    }
-    res.render("index", { errorMessage: "Hey that email is already taken" });
-  });
+  const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+  if (regex.test(password)) {
+    User.find({ email }).then((userFromDB) => {
+      // []
+      if (!userFromDB.length) {
+        const hashedPassword = bcrypt.hashSync(password, 10);
+        User.create({ username, password: hashedPassword, email })
+          .then((responseFromDB) => {
+            res.render("index", responseFromDB);
+          })
+          .catch((err) => {
+            console.log(err);
+            if (err.code === 11000) {
+              res.render("index", {
+                errorMessage: "Hey that username is already taken",
+              });
+            } else {
+              res.render("index", {
+                errorMessage: err.message,
+              });
+            }
+          });
+      } else {
+        res.render("index", {
+          errorMessage: "Hey that email is already taken",
+        });
+      }
+    });
+  } else {
+    res.render("index", {
+      errorMessage:
+        "Please make sure the password has min 6 characters, at least one lower, Upper and numeric",
+    });
+  }
 });
 
 module.exports = router;
